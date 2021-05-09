ff6a98b21b55547bd4060d553807ba0e2aef4b54
CodeCanna <codecannamw@gmail.com>
2021-05-09 08:47:06 -0600

    I got the about me sectio to slide open and closed using Jquery.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
+window.onload = () => {
+    $('#about-me').hide();
+}
+
 // Menu is initially closed
 var menuShown = false;
 
 $('#about-btn-show').on('click', () => {
     if (! menuShown) {
         menuShown = true;
-        $('#about-me').removeClass('is-hidden');
+        $('#about-me').slideDown()
         $('#about-btn-show span').css('background-color', 'silver');
     } else {
         menuShown = false;
-        $('#about-me').addClass('is-hidden');
+        $('#about-me').slideUp();
         $('#about-btn-show span').css('background-color', 'transparent');
     }
     $('#about-btn-show').toggleClass("aboutMeBtnClicked");
